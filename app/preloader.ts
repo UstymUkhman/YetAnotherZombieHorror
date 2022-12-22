039b100c560527bbaf109a122d381df6e766fdbd
Ustym <ustym.ukhman@tbwa-x.com>
2022-12-22 12:02:03 +0200

    Update Settigns Tests

--- app/preloader.ts
+++ app/preloader.ts
@@ -1,12 +1,12 @@
 import { contextBridge, ipcRenderer } from 'electron';
-import { version, dependencies } from '../package.json';
+// import { version, dependencies } from '../package.json';
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.info(`App      : v${version}`);
+  // console.info(`App      : v${version}`);
   console.info(`Node     : v${process.versions.node}`);
   console.info(`Chrome   : v${process.versions.chrome}`);
   console.info(`Electron : v${process.versions.electron}`);
-  console.info(`three.js : r${dependencies.three.slice(1)}`);
+  // console.info(`three.js : r${dependencies.three.slice(1)}`);
 });
 
 contextBridge.exposeInMainWorld('exit', () => ipcRenderer.sendSync('exit'));
--- tests/Settings.spec.ts
+++ tests/Settings.spec.ts
@@ -1,10 +1,32 @@
+import Performance from '@/settings/performance.json';
+
 import { describe, test, expect } from 'vitest';
 import Physics from '@/settings/physics.json';
 
 import Settings from '@/settings';
 import Configs from '@/configs';
 
+import {
+  Quality,
+  MAX_CLOUDS,
+  DEFAULT_QUALITY,
+  PERFORMANCE_LENGTH,
+  DefaultPerformance
+} from '@/settings/constants';
+
 describe('Settings', () => {
+  test('Constants', () => {
+    expect(DefaultPerformance).toStrictEqual(Performance[0]);
+    expect(PERFORMANCE_LENGTH).toStrictEqual(13.0);
+
+    expect(DEFAULT_QUALITY).toStrictEqual(0.0);
+    expect(MAX_CLOUDS).toStrictEqual(300.0);
+
+    expect(Quality.LOW).toStrictEqual(13.0);
+    expect(Quality.MEDIUM).toStrictEqual(14.0);
+    expect(Quality.HIGH).toStrictEqual(15.0);
+  });
+
   test('Performance', () => {
     expect(typeof Settings.getPerformanceValue('bullet')).toStrictEqual('boolean');
     expect(typeof Settings.getPerformanceValue('bulletPath')).toStrictEqual('boolean');
@@ -24,6 +46,63 @@ describe('Settings', () => {
     expect(typeof Settings.getPerformanceValue('clouds')).toStrictEqual('number');
     expect(typeof Settings.getPerformanceValue('dynamicClouds')).toStrictEqual('boolean');
     expect(typeof Settings.getPerformanceValue('physicalLights')).toStrictEqual('boolean');
+  });
+
+  test('Performance.LOW', () => {
+    expect(Performance[0].bullet).toStrictEqual(true);
+    expect(Performance[0].bulletPath).toStrictEqual(false);
+    expect(Performance[0].bulletHoles).toStrictEqual(false);
+
+    expect(Performance[0].raining).toStrictEqual(true);
+    expect(Performance[0].lighting).toStrictEqual(false);
+    expect(Performance[0].raindrops).toStrictEqual(false);
+    expect(Performance[0].softParticles).toStrictEqual(false);
+
+    expect(Performance[0].fog).toStrictEqual(true);
+    expect(Performance[0].bakedFog).toStrictEqual(false);
+    expect(Performance[0].volumetricFog).toStrictEqual(false);
+
+    expect(Performance[0].clouds).toStrictEqual(0.0);
+    expect(Performance[0].dynamicClouds).toStrictEqual(false);
+    expect(Performance[0].physicalLights).toStrictEqual(false);
+  });
+
+  test('Performance.MEDIUM', () => {
+    expect(Performance[1].bullet).toStrictEqual(true);
+    expect(Performance[1].bulletPath).toStrictEqual(false);
+    expect(Performance[1].bulletHoles).toStrictEqual(true);
+
+    expect(Performance[1].raining).toStrictEqual(true);
+    expect(Performance[1].lighting).toStrictEqual(true);
+    expect(Performance[1].raindrops).toStrictEqual(true);
+    expect(Performance[1].softParticles).toStrictEqual(false);
+
+    expect(Performance[1].fog).toStrictEqual(false);
+    expect(Performance[1].bakedFog).toStrictEqual(false);
+    expect(Performance[1].volumetricFog).toStrictEqual(false);
+
+    expect(Performance[1].clouds).toStrictEqual(300.0);
+    expect(Performance[1].dynamicClouds).toStrictEqual(true);
+    expect(Performance[1].physicalLights).toStrictEqual(false);
+  });
+
+  test('Performance.HIGH', () => {
+    expect(Performance[2].bullet).toStrictEqual(true);
+    expect(Performance[2].bulletPath).toStrictEqual(true);
+    expect(Performance[2].bulletHoles).toStrictEqual(true);
+
+    expect(Performance[2].raining).toStrictEqual(true);
+    expect(Performance[2].lighting).toStrictEqual(true);
+    expect(Performance[2].raindrops).toStrictEqual(true);
+    expect(Performance[2].softParticles).toStrictEqual(true);
+
+    expect(Performance[2].fog).toStrictEqual(true);
+    expect(Performance[2].bakedFog).toStrictEqual(true);
+    expect(Performance[2].volumetricFog).toStrictEqual(true);
+
+    expect(Performance[2].clouds).toStrictEqual(1.0);
+    expect(Performance[0].dynamicClouds).toStrictEqual(false);
+    expect(Performance[2].physicalLights).toStrictEqual(true);
   });
 
   test('Enemy', () => {
